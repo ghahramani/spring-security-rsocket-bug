46dd167bf69c7ebe4b441271123c18a5ddcd7157
Navid Ghahremani <nghahremani@mimecast.com>
2021-01-14 18:33:31 +0000

    added working and not-working endpoint

--- src/main/kotlin/com/example/demo/TestResource.kt
+++ src/main/kotlin/com/example/demo/TestResource.kt
@@ -8,6 +8,7 @@ import org.springframework.messaging.rsocket.RSocketRequester
 import org.springframework.messaging.rsocket.retrieveFlux
 import org.springframework.util.MimeType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,7 +19,6 @@ import org.springframework.web.reactive.function.client.bodyToFlux
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import java.io.Serializable
 
 @RestController
 @RequestMapping
@@ -32,14 +32,14 @@ class TestResource {
     private lateinit var webClient: WebClient
 
     @PostMapping("/api/v2/uploads.json", consumes = [MediaType.ALL_VALUE])
-    fun aa(@RequestParam filename: String, @RequestBody(required = false) data: ByteArray?): Result {
-        println("New Data: ${data?.toString(Charsets.UTF_8)}")
+    fun resource(@RequestParam filename: String, @RequestBody data: ByteArray): Result {
+        println("New Data: ${data.toString(Charsets.UTF_8)}")
         println("Filename: $filename")
-        return Result(listOf(Attachment(20, filename)))
+        return Result(filename, data.toString(Charsets.UTF_8))
     }
 
-    @MessageMapping("test")
-    fun aa(@Payload data: Flux<ByteArray>): Flux<UploadResponseVM> =
+    @MessageMapping("working")
+    fun working(@Payload data: Flux<ByteArray>) =
         webClient
             .post()
             .uri(
@@ -53,21 +53,34 @@ class TestResource {
             .body(Flux.just("Test", "Test").map { it.encodeToByteArray() }, ByteArray::class.java)
             .retrieve()
             .bodyToFlux<Result>()
-            .map { UploadResponseVM(it.attachments.first().id.toString()) }
 
-    @GetMapping("/test-upload")
-    fun test() = requester
+    @MessageMapping("not-working")
+    fun notWorking(@Payload data: Flux<ByteArray>) =
+        webClient
+            .post()
+            .uri(
+                UriComponentsBuilder
+                    .fromUriString("http://localhost:9999/api/v2/uploads.json")
+                    .queryParam("filename", "TestFileName")
+                    .build(true)
+                    .toUri()
+            )
+            .contentType(MediaType("application", "binary"))
+            .body(data, ByteArray::class.java)
+            .retrieve()
+            .bodyToFlux<Result>()
+
+    @GetMapping("/{route}")
+    fun notWorkingTest(@PathVariable route: String) = requester
         .map { client ->
             client
-                .route("test")
+                .route(route)
                 .metadata("test", MimeType.valueOf("message/x.file.name"))
                 .data(Flux.just("data", "test", "dat2").map { it.encodeToByteArray() })
         }
-        .flatMapMany { client -> client.retrieveFlux<UploadResponseVM>() }
-
+        .flatMapMany { client -> client.retrieveFlux<Result>() }
+        .onErrorResume { Flux.just(Result("not-passed", it.message ?: "No Error Message")) }
 
 }
 
-data class UploadResponseVM(val attachmentId: String) : Serializable
-data class Attachment(val id: Long, val fileName: String)
-data class Result(val attachments: List<Attachment>)
+data class Result(val fileName: String, val body: String)
